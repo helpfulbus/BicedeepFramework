e7f69d86c57ecea9449d9b41804d683e15a5b930
Berk Eserol <berkeserol@gmail.com>
2018-03-25 18:13:20 +0300

    report docker paths

--- ReportDocker.py
+++ ReportDocker.py
@@ -6,11 +6,12 @@ import sys
 from ReportServerAI import report
 from ReportServerAI import optimization
 
-filename = sys.argv[1]
+filepath = sys.argv[1]
+filename = filepath.split('/')[-1]
 selectedHeaders = []
 
 for i in range(2, len(sys.argv)):
     selectedHeaders.append(sys.argv[i])
 
-report.create_report(file_path, file_name, selected_headers, reports_path, outputs_path, email, file_name_full)
-optimization.do_optimization(file_path, file_name, reports_path, outputs_path, email, file_name_full)
+report.create_report(filepath, filename, selectedHeaders, './filesystem/reports', './filesystem/models', '', filename)
+optimization.do_optimization(filepath, filename, './filesystem/reports', './filesystem/models', '', filename)
