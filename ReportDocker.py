b0bad7c34f54f2a671ec2b16442af2fe5262db9f
Berk Eserol <berkeserol@gmail.com>
2018-06-22 13:55:54 +0300

    report docker start methods

--- ReportDocker.py
+++ ReportDocker.py
@@ -3,15 +3,40 @@
 # </copyright>
 
 import sys
+import datetime
 from ReportServerAI import report
 from ReportServerAI import optimization
 
-filepath = sys.argv[1]
-filename = filepath.split('/')[-1]
+print("filename : " + sys.argv[1])
+filename = sys.argv[1]
+filepath = "./filesystem/data/" + filename
 selectedHeaders = []
 
 for i in range(2, len(sys.argv)):
     selectedHeaders.append(sys.argv[i])
 
-report.create_report(filepath, filename, selectedHeaders, './filesystem/reports', './filesystem/models', '', filename)
-optimization.do_optimization(filepath, filename, './filesystem/reports', './filesystem/models', '', filename)
+f= open("./filesystem/logs/" + filename + "/status.log","w")
+
+f.write(str(datetime.datetime.now()) + " Create Report Filtering Started\n")
+f.flush()
+
+try:
+    report.create_report(filepath, filename, selectedHeaders, './filesystem/reports', './filesystem/models', '',filename)
+except Exception as e:
+    f.write(str(datetime.datetime.now()) + str(e))
+    f.flush()
+
+f.write(str(datetime.datetime.now()) + " Create Report Filtering Completed\n")
+f.write(str(datetime.datetime.now()) + " Optimization Started\n")
+f.flush()
+
+try:
+    optimization.do_optimization(filepath, filename, './filesystem/reports', './filesystem/models', '', filename)
+except Exception as e:
+    f.write(str(datetime.datetime.now()) + str(e))
+    f.flush()
+
+f.write(str(datetime.datetime.now()) + " Optimization Completed\n")
+f.flush()
+
+f.close()
